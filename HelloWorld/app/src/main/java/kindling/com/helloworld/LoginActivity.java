2af38f6849c3d8502da67474757af7aca66f285e
Trevor Cai <tycai29@gmail.com>
2015-04-19 16:44:18 -0700

    Login button leads to MainActivity

--- HelloWorld/app/src/main/java/kindling/com/helloworld/LoginActivity.java
+++ HelloWorld/app/src/main/java/kindling/com/helloworld/LoginActivity.java
@@ -1,9 +1,12 @@
 package kindling.com.helloworld;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 
 public class LoginActivity extends ActionBarActivity {
@@ -14,5 +17,17 @@ public class LoginActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         setTitle(getString(R.string.login));
+
+        // Detect button click for signing in
+        // detect login button click
+        ImageButton loginButton = (ImageButton) findViewById(R.id.actualLoginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
