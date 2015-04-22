85d2ca590c16985eb4853dde1c1d1afc855735d8
Joseph Goelz <jgoelz@usc.edu>
2015-04-21 23:17:33 -0700

    Jay Dee adding the infrastructure for the model component of project. Namely the User and Range classes

---
+++ HelloWorld/app/src/main/java/model/kindling/Range.java
@@ -0,0 +1,7 @@
+package model.kindling;
+
+/**
+ * Created by Jay on 4/21/2015.
+ */
+public class Range {
+}
---
+++ HelloWorld/app/src/main/java/model/kindling/User.java
@@ -0,0 +1,10 @@
+package model.kindling;
+//the model package will handle the apps data
+/**
+ * Created by Jay on 4/21/2015.
+ */
+public class User {
+    //the user class as described in the DDD
+
+
+}
