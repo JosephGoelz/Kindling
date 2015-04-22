f322f192d45160ea707f3f4fabfde2696d524819
TAEGYUM KIM <taegyumkim@usc-secure-wireless-045-050.usc.edu>
2015-04-21 21:28:07 -0700

    matching screen

---
+++ HelloWorld/app/src/main/java/kindling/com/helloworld/MatchingActivity.java
@@ -0,0 +1,22 @@
+package kindling.com.helloworld;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MatchingActivity extends ActionBarActivity  {
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maching);
+        setTitle("Matching");
+
+
+    }
+}
