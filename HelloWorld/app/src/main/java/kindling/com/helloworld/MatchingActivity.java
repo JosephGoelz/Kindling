3f87102eafaac8140b68a378f395f363dd62dfa5
Winston Lee <winstojl@usc.edu>
2015-04-21 21:52:26 -0700

    correcting incorrect file name

--- HelloWorld/app/src/main/java/kindling/com/helloworld/MatchingActivity.java
+++ HelloWorld/app/src/main/java/kindling/com/helloworld/MatchingActivity.java
@@ -1,20 +1,14 @@
 package kindling.com.helloworld;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 
 public class MatchingActivity extends ActionBarActivity  {
 
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maching);
+        setContentView(R.layout.activity_matching);
         setTitle("Matching");
 
 
