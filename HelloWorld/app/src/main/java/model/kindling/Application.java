dbb55c215467cb63c978a3c99c2aeddeb83b67aa
Joyce Yan <joyceyan@usc.edu>
2015-04-24 22:16:30 -0700

    updated application to behave as a singleton

--- HelloWorld/app/src/main/java/model/kindling/Application.java
+++ HelloWorld/app/src/main/java/model/kindling/Application.java
@@ -4,9 +4,8 @@ package model.kindling;
  * Created by Jay on 4/21/2015.
  */
 public class Application {
-    private User _User;
-    private boolean loggedIn;
-
+    private static User _User;
+    private static boolean loggedIn;
 
     public Application(User _User){
         //when we log in we give the application our user
@@ -15,20 +14,16 @@ public class Application {
         loggedIn = true;
     }
 
-    private void logOut(){
+    public static void logOut(){
         //we only need logOut because we log in as soon as the application
         //starts by default
         loggedIn = false;
     }
-    public boolean isLoggedIn() {
+    public static boolean isLoggedIn() {
         return loggedIn;
     }
 
-    public User get_User() {
+    public static User getUser() {
         return _User;
-    }
-
-    public void set_User(User _User) {
-        this._User = _User;
     }
 }
