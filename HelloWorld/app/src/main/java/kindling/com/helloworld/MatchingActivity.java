db8ff7cb4f9ac96ae49f7c4c8e8def0c9ce93127
TAEGYUM KIM <taegyumkim@TAEGYUMs-MacBook-Pro.local>
2015-04-25 00:05:10 -0700

    popup added

--- HelloWorld/app/src/main/java/kindling/com/helloworld/MatchingActivity.java
+++ HelloWorld/app/src/main/java/kindling/com/helloworld/MatchingActivity.java
@@ -7,6 +7,16 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.graphics.drawable.ColorDrawable;
+
+
+
 
 
 public class MatchingActivity extends ActionBarActivity {
@@ -24,10 +34,35 @@ public class MatchingActivity extends ActionBarActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(), MainMessagesActivity.class);
                 startActivity(intent);
+
             }
+        });
+        ImageButton acceptbtn;
+        acceptbtn = (ImageButton) findViewById(R.id.rightButton);
+        acceptbtn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //put boolean variable
+                popUp();
+            }
+
         });
 
     }
 
 
+
+
+    private void popUp() {
+
+            ImageView image = new ImageView(this);
+            image.setImageResource(R.drawable.its_a_match);
+
+            AlertDialog.Builder picturePopUp = new AlertDialog.Builder(this);
+            picturePopUp.setView(image);
+
+            AlertDialog helpDialog = picturePopUp.create();
+            helpDialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+            helpDialog.show();
+    }
 }
