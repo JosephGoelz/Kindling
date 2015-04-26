c3409feb31041acc1e09cc877b3e325e030499d1
Joyce Yan <joyceyan@usc.edu>
2015-04-26 00:09:36 -0700

    added structure for settings page

--- HelloWorld/app/src/main/java/kindling/com/helloworld/SettingsActivity.java
+++ HelloWorld/app/src/main/java/kindling/com/helloworld/SettingsActivity.java
@@ -12,6 +12,7 @@ public class SettingsActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        setTitle("Settings");
     }
 
 
