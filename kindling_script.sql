143d8a9d1e221fde68eaa99dc7f64bb868cb195f
Joseph Goelz <jgoelz@usc.edu>
2015-04-26 13:01:09 -0700

    Added matches, encountered, and nos field to database

--- kindling_script.sql
+++ kindling_script.sql
@@ -1,6 +1,6 @@
-DROP DATABASE if exists bindling;
-CREATE DATABASE bindling;
-USE bindling;
+DROP DATABASE if exists kindling;
+CREATE DATABASE kindling;
+USE kindling;
 
 CREATE TABLE users
 (
@@ -16,146 +16,189 @@ start_intel_pref int not null,
 finish_intel_pref int not null,
 age_pref_start int not null,
 age_pref_finish int not null,
-City varchar(255)
+city varchar(255),
+matches varchar(500),
+nos varchar(500),
+encountered varchar(500)
 );
 
 
 INSERT INTO users (gender, sexualOrientation, name, username,
 location, password, age, intelligence, city,
 start_intel_pref, finish_intel_pref,
-age_pref_start, age_pref_finish) VALUES (1, 0, 'Barbara',
+age_pref_start, age_pref_finish,
+matches, nos, encountered) VALUES (1, 0, 'Barbara',
 'BMoney', 'Tibet', 'password', 21, 50, 'Lhasa',
-40, 80, 20, 30);
+40, 80, 20, 30,
+'LMoney,MMoney', '', '');
 
 INSERT INTO users (gender, sexualOrientation, name, username,
 location, password, age, intelligence, city,
 start_intel_pref, finish_intel_pref,
-age_pref_start, age_pref_finish) VALUES (1, 0, 'Chelsea',
+age_pref_start, age_pref_finish,
+matches, nos, encountered) VALUES (1, 0, 'Chelsea',
 'CMoney', 'Tibet', 'password', 21, 50, 'Lhasa',
-40, 80, 20, 30);
+40, 80, 20, 30,
+'LMoney,MMoney', '', '');
 
 INSERT INTO users (gender, sexualOrientation, name, username,
 location, password, age, intelligence, city,
 start_intel_pref, finish_intel_pref,
-age_pref_start, age_pref_finish) VALUES (1, 0, 'Danica',
+age_pref_start, age_pref_finish,
+matches, nos, encountered) VALUES (1, 0, 'Danica',
 'DMoney', 'Tibet', 'password', 21, 50, 'Lhasa',
-40, 80, 20, 30);
+40, 80, 20, 30,
+'', '', '');
 
 INSERT INTO users (gender, sexualOrientation, name, username,
 location, password, age, intelligence, city,
 start_intel_pref, finish_intel_pref,
-age_pref_start, age_pref_finish) VALUES (1, 0, 'Elanor',
+age_pref_start, age_pref_finish,
+matches, nos, encountered) VALUES (1, 0, 'Elanor',
 'EMoney', 'Tibet', 'password', 21, 50, 'Lhasa',
-40, 80, 20, 30);
+40, 80, 20, 30,
+'', '', '');
 
 INSERT INTO users (gender, sexualOrientation, name, username,
 location, password, age, intelligence, city,
 start_intel_pref, finish_intel_pref,
-age_pref_start, age_pref_finish) VALUES (1, 0, 'Fannie',
+age_pref_start, age_pref_finish,
+matches, nos, encountered) VALUES (1, 0, 'Fannie',
 'FMoney', 'Tibet', 'password', 21, 50, 'Lhasa',
-40, 80, 20, 30);
+40, 80, 20, 30,
+'', '', '');
 
 INSERT INTO users (gender, sexualOrientation, name, username,
 location, password, age, intelligence, city,
 start_intel_pref, finish_intel_pref,
-age_pref_start, age_pref_finish) VALUES (1, 0, 'Georgia',
+age_pref_start, age_pref_finish,
+matches, nos, encountered) VALUES (1, 0, 'Georgia',
 'GMoney', 'Tibet', 'password', 21, 50, 'Lhasa',
-40, 80, 20, 30);
+40, 80, 20, 30,
+'', '', '');
 
 INSERT INTO users (gender, sexualOrientation, name, username,
 location, password, age, intelligence, city,
 start_intel_pref, finish_intel_pref,
-age_pref_start, age_pref_finish) VALUES (1, 0, 'Hillary',
+age_pref_start, age_pref_finish,
+matches, nos, encountered) VALUES (1, 0, 'Hillary',
 'HMoney', 'Tibet', 'password', 21, 50, 'Lhasa',
-40, 80, 20, 30);
+40, 80, 20, 30,
+'', '', '');
 
 INSERT INTO users (gender, sexualOrientation, name, username,
 location, password, age, intelligence, city,
 start_intel_pref, finish_intel_pref,
-age_pref_start, age_pref_finish) VALUES (1, 0, 'Ilsa',
+age_pref_start, age_pref_finish,
+matches, nos, encountered) VALUES (1, 0, 'Ilsa',
 'IMoney', 'Tibet', 'password', 21, 50, 'Lhasa',
-40, 80, 20, 30);
+40, 80, 20, 30,
+'', '', '');
 
 INSERT INTO users (gender, sexualOrientation, name, username,
 location, password, age, intelligence, city,
 start_intel_pref, finish_intel_pref,
-age_pref_start, age_pref_finish) VALUES (1, 1, 'Janice',
+age_pref_start, age_pref_finish,
+matches, nos, encountered) VALUES (1, 1, 'Janice',
 'JMoney', 'Tibet', 'password', 21, 50, 'Lhasa',
-40, 80, 20, 30);
+40, 80, 20, 30,
+'', '', '');
 
 INSERT INTO users (gender, sexualOrientation, name, username,
 location, password, age, intelligence, city,
 start_intel_pref, finish_intel_pref,
-age_pref_start, age_pref_finish) VALUES (1, 1, 'Kathy',
+age_pref_start, age_pref_finish,
+matches, nos, encountered) VALUES (1, 1, 'Kathy',
 'KMoney', 'Tibet', 'password', 21, 50, 'Lhasa',
-40, 80, 20, 30);
+40, 80, 20, 30,
+'', '', '');
 
 INSERT INTO users (gender, sexualOrientation, name, username,
 location, password, age, intelligence, city,
 start_intel_pref, finish_intel_pref,
-age_pref_start, age_pref_finish) VALUES (0, 1, 'Lenny',
+age_pref_start, age_pref_finish,
+matches, nos, encountered) VALUES (0, 1, 'Lenny',
 'LMoney', 'Tibet', 'password', 21, 50, 'Lhasa',
-40, 80, 20, 30);
+40, 80, 20, 30,
+'BMoney,CMoney', '', '');
 
 INSERT INTO users (gender, sexualOrientation, name, username,
 location, password, age, intelligence, city,
 start_intel_pref, finish_intel_pref,
-age_pref_start, age_pref_finish) VALUES (0, 1, 'Maurice',
+age_pref_start, age_pref_finish,
+matches, nos, encountered) VALUES (0, 1, 'Maurice',
 'MMoney', 'Tibet', 'password', 21, 50, 'Lhasa',
-40, 80, 20, 30);
+40, 80, 20, 30,
+'BMoney,CMoney', '', '');
 
 INSERT INTO users (gender, sexualOrientation, name, username,
 location, password, age, intelligence, city,
 start_intel_pref, finish_intel_pref,
-age_pref_start, age_pref_finish) VALUES (0, 1, 'Ned',
+age_pref_start, age_pref_finish,
+matches, nos, encountered) VALUES (0, 1, 'Ned',
 'NMoney', 'Tibet', 'password', 21, 50, 'Lhasa',
-40, 80, 20, 30);
+40, 80, 20, 30,
+'', '', '');
 
 INSERT INTO users (gender, sexualOrientation, name, username,
 location, password, age, intelligence, city,
 start_intel_pref, finish_intel_pref,
-age_pref_start, age_pref_finish) VALUES (0, 1, 'Oscar',
+age_pref_start, age_pref_finish,
+matches, nos, encountered) VALUES (0, 1, 'Oscar',
 'OMoney', 'Tibet', 'password', 21, 50, 'Lhasa',
-40, 80, 20, 30);
+40, 80, 20, 30,
+'', '', '');
 
 INSERT INTO users (gender, sexualOrientation, name, username,
 location, password, age, intelligence, city,
 start_intel_pref, finish_intel_pref,
-age_pref_start, age_pref_finish) VALUES (0, 1, 'Philip',
+age_pref_start, age_pref_finish,
+matches, nos, encountered) VALUES (0, 1, 'Philip',
 'PMoney', 'Tibet', 'password', 21, 50, 'Lhasa',
-40, 80, 20, 30);
+40, 80, 20, 30,
+'', '', '');
 
 INSERT INTO users (gender, sexualOrientation, name, username,
 location, password, age, intelligence, city,
 start_intel_pref, finish_intel_pref,
-age_pref_start, age_pref_finish) VALUES (0, 1, 'Quinn',
+age_pref_start, age_pref_finish,
+matches, nos, encountered) VALUES (0, 1, 'Quinn',
 'QMoney', 'Tibet', 'password', 21, 50, 'Lhasa',
-40, 80, 20, 30);
+40, 80, 20, 30,
+'', '', '');
 
 INSERT INTO users (gender, sexualOrientation, name, username,
 location, password, age, intelligence, city,
 start_intel_pref, finish_intel_pref,
-age_pref_start, age_pref_finish) VALUES (0, 1, 'Red',
+age_pref_start, age_pref_finish,
+matches, nos, encountered) VALUES (0, 1, 'Red',
 'RMoney', 'Tibet', 'password', 21, 50, 'Lhasa',
-40, 80, 20, 30);
+40, 80, 20, 30,
+'', '', '');
 
 INSERT INTO users (gender, sexualOrientation, name, username,
 location, password, age, intelligence, city,
 start_intel_pref, finish_intel_pref,
-age_pref_start, age_pref_finish) VALUES (0, 1, 'Samuel',
+age_pref_start, age_pref_finish,
+matches, nos, encountered) VALUES (0, 1, 'Samuel',
 'SMoney', 'Tibet', 'password', 21, 50, 'Lhasa',
-40, 80, 20, 30);
+40, 80, 20, 30,
+'', '', '');
 
 INSERT INTO users (gender, sexualOrientation, name, username,
 location, password, age, intelligence, city,
 start_intel_pref, finish_intel_pref,
-age_pref_start, age_pref_finish) VALUES (0, 0, 'Theo',
+age_pref_start, age_pref_finish,
+matches, nos, encountered) VALUES (0, 0, 'Theo',
 'TMoney', 'Tibet', 'password', 21, 50, 'Lhasa',
-40, 80, 20, 30);
+40, 80, 20, 30,
+'', '', '');
 
 INSERT INTO users (gender, sexualOrientation, name, username,
 location, password, age, intelligence, city,
 start_intel_pref, finish_intel_pref,
-age_pref_start, age_pref_finish) VALUES (0, 0, 'Ulyses',
+age_pref_start, age_pref_finish,
+matches, nos, encountered) VALUES (0, 0, 'Ulyses',
 'UMoney', 'Tibet', 'password', 21, 50, 'Lhasa',
-40, 80, 20, 30);
+40, 80, 20, 30,
+'', '', '');
