a63b444548b3ee8074c65134cf7892eadc6af1d4
Taegyum Kim <taegyumk@usc.edu>
2015-04-26 19:07:32 -0700

    word game timer

--- HelloWorld/app/src/main/java/kindling/com/helloworld/WordGameActivity.java
+++ HelloWorld/app/src/main/java/kindling/com/helloworld/WordGameActivity.java
@@ -6,12 +6,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class WordGameActivity extends ActionBarActivity {
 
     Button answer_one, answer_two, answer_three, answer_four;
     TextView question_text;
+    int counter = 30;
+    String string_Counter;
+    TextView timerUpdate;
 
     public static final String question = "Question";
     public static final String answer1 = "Answer1";
@@ -30,6 +35,7 @@ public class WordGameActivity extends ActionBarActivity {
 
 
         question_text = (TextView) findViewById(R.id.wordGame_questionText);
+        timerUpdate = (TextView) findViewById(R.id.wordGame_counter);
 
         answer_one = (Button) findViewById(R.id.wordGame_AnswerButton1);
         answer_two = (Button) findViewById(R.id.wordGame_AnswerButton2);
@@ -70,7 +76,30 @@ public class WordGameActivity extends ActionBarActivity {
 
 
     }
+    // when the game is started, call this function, then count starts
+    public void timeCounter() {
+        Timer time = new Timer();
+        TimerTask task = new TimerTask(){
+            public void run() {
 
+                string_Counter = String.valueOf(counter);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        timerUpdate.setText("0:"+string_Counter);
+
+                    }
+                });
+                counter--;
+                if(counter == 0) {
+                    //when counter is 0, then next game pops up
+                    counter = 30;
+                }
+
+            }
+        };
+        time.schedule(task, 1000,1000);
+    }
 
 
 
