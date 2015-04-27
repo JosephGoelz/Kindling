255e3e9b7db305ec56bbd16f0f66a85d6532ba94
Joseph Goelz <jgoelz@usc.edu>
2015-04-26 17:32:21 -0700

    Added Question classes, some work might be needed

---
+++ HelloWorld/app/src/main/java/model/kindling/Question.java
@@ -0,0 +1,33 @@
+package model.kindling;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Jay on 4/26/2015.
+ */
+public abstract class Question {
+    public String questionText;
+    public String correctAnswer;
+    public int correctAnswerIndex;
+    public String aAnswer, bAnswer, cAnswer, dAnswer;
+    //indexed as 0, 1, 2, 3
+
+
+    public abstract void populateAnswers(); //will  set all answers to random
+
+    public abstract void generateQuestionText();
+
+    public void correctAnswer(){
+        //increments intelligence rating by one
+        //runs animation thread
+    }
+
+    public void incorrectAnswer(){
+        //decrements intelligence rating by one
+        //runs animation thread
+    }
+
+    public abstract void setCorrectAnswer(int index, String correctAnswer);
+    //chooses a correctAnswer index and overrides
+    //the option text with the correctanswer
+}
